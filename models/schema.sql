b5fa5fad9d6f172908fbccfd7d496e0fdf31754c
MariaDawes <maria.c.dawes@gmail.com>
2020-02-05 12:20:18 -0600

    data added to schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,3 +1,35 @@
 DROP DATABASE IF EXISTS movie_review_db;
 CREATE DATABASE movie_review_db;
 USE movie_review_db;
+
+INSERT INTO reviews (review, title, score, )
+VALUES (“Frozen”, “Sisters do cool things.“, 7.5);
+INSERT INTO reviews (title, review, score)
+VALUES (“Frozen”, “Typical Disney soundtrack. Catchy songs and colorful characters. Refreshing that the movie is about a strong woman who doesn’t need a prince to rescue her.“, 8);
+INSERT INTO reviews (title, review, score)
+VALUES (“Frozen”, “Interesting concept. Good story. How many super powers does Elsa have? She can create ice, make ice castles, and create living beings.“, 6);
+INSERT INTO reviews (title, review, score)
+VALUES (“Star Wars: The Rise of Skywalker”, “As with all the other Star Wars movies, this one did not disappoint. It was action packed, full of familiar faces and just very well done. I would watch it again!“, 9);
+INSERT INTO reviews (title, review, score)
+VALUES (“Star Wars: The Rise of Skywalker”, “Excellent movie! Great sequel for the Star Wars Saga!“, 9.8);
+INSERT INTO reviews (title, review, score)
+VALUES (“Star Wars: The Rise of Skywalker”, “I almost slept! Terrible movie! Not worth spending you time and money!“, 2);
+INSERT INTO reviews (title, review, score)
+VALUES (“The Matrix”, “With a visionary direction and script by the Wachowskis, a beautiful soundtrack by Don Davis, perfect visual effects, striking action scenes, cutting-edge cinematography by Bill Pope, a fantastic editing by Zach Staenberg and excellent performances by Keanu Reeves, Laurence Fishburne, Carrie- Anne Moss and Hugo Weaving, The Matrix manages to be a remarkable film, one of the best science fiction films of all time and one of the best films of the 90s”, 9);
+INSERT INTO reviews (title, review, score)
+VALUES (“The Matrix”, “Keanu Reeves is brilliant along with all the cast! Amazing movie!”, 9);
+INSERT INTO reviews (title, review, score)
+VALUES (“Four weddings and a funeral“, “Best movie ever!“ 9.2);
+INSERT INTO reviews (title, review, score)
+VALUES (“Nemo”, “Perfect movie!“, 9.3);
+
+
+
+INSERT INTO users (email, password)
+VALUES (“aaa@gmail.com”, aaaaa)
+INSERT INTO users (email, password)
+VALUES (“mmm@gmail.com”, mmmmm)
+INSERT INTO users (email, password)
+VALUES (“bbb@gmail.com”, bbbbb)
+INSERT INTO users (email, password)
+VALUES (“ccc@gmail.com”, ccccc)
