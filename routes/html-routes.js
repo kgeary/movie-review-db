d7ff97f6f953708a2d7dbaa124c615ee00393328
Kevin Geary <kgeary@gmail.com>
2020-02-04 19:00:43 -0600

    see if the user is logged in. still a WIP

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,13 +6,13 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
 
-  app.get("/", function (req, res) {
+  app.get(["/", "/index"], function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     //res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render("index", { msg: "Hi there!" });
+    res.render("index", { msg: "Hi there!", user: req.user });
   });
 
   app.get("/members", function (req, res) {
@@ -22,15 +22,8 @@ module.exports = function (app) {
   app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/index");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
 };
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
-var ejs = require("ejs");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
