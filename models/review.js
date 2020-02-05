105eb829e7b138f09347d50de31bb2e0e1cf000b
MariaDawes <maria.c.dawes@gmail.com>
2020-02-05 11:49:04 -0600

    files review adn user done

--- models/review.js
+++ models/review.js
@@ -1,19 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Review = sequelize.define("Review", {
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
     review: {
       type: DataTypes.STRING,
-      // Shoud I use type: DataTypes.TEXT instead? 
       allowNull: false,
       validate: {
         len: [300]
       }
-     }
+    },
+    score: {
+      type: DataTypes.DECIMAL(3, 1),
+      allowNull: false,
+      validate: {
+        isDecimal
+      }
+    },
+    title: {
+      type: DataTypes.STRING
+    }
   });
-
   Review.associate = function (models) {
     models.Review.belongsTo(models.User, {
       onDelete: "CASCADE",
@@ -21,6 +25,11 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       }
     });
-  };  
+  };
   return Review;
 };
+
+
+
+
+
