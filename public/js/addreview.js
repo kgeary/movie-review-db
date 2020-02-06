8966bcdd2d9c96d4a2f495117a4dceda752d0c31
Kevin Geary <kgeary@gmail.com>
2020-02-06 12:01:58 -0600

    WIP

---
+++ public/js/addreview.js
@@ -0,0 +1,23 @@
+$("#add-review").on("click", function(event) {
+    event.preventDefault();
+  var newReview = {
+  title: $("#title").val().trim(),
+rating: $("#score").val().trim(),
+  review: $("#review").val().trim(),
+
+};
+
+$.post("/api/reviews", newReview)
+    // on success, run this callback
+    .then(function(data) {
+      // log the data we found
+      console.log(data);
+      alert("Adding review...");
+    });
+
+  // empty each input box by replacing the value with an empty string
+  $("#title").val("");
+  $("#score").val("");
+  $("#review").val("");
+ 
+});
--- routes/reviews-api-routes.js
+++ routes/reviews-api-routes.js
@@ -1,34 +1,61 @@
 var db = require("../models");
 
+function FindMovieID(title, user) {
+  db.Movie.findOne({ where: { title: title } })
+    .then(rows => {
+      var movieId;
+      if (rows.length < 1) {
+        // NEW MOVIE
+        movieId = await addNewMovie(title)
+      } else {
+        movieId = rows[0].id;
+      }
+    });
+}
+
+async function addNewMovie(title) {
+  // queryombd
+  axios.get(query + title).then((data) => {
+    db.Movie.create({
+      title: data.Title,
+      rating: data.Rating,
+      img: data.Poster;
+      year: data.Year;
+    }).then((rec) => { return rec.id });
+  });
+}
+
+
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the reviews
-  app.get("/api/reviews", function(req, res) {
+  app.get("/api/reviews", function (req, res) {
     var query = {};
     if (req.query.user_id) {
       query.UserId = req.query.user_id;
     }
     // 1. Add a join here to include all of the users to these reviews
     db.Review.findAll({
-      include:{
-        model:db.User
-      }}).then(function(dbReview) {
+      include: {
+        model: db.User
+      }
+    }).then(function (dbReview) {
       res.json(dbReview);
     });
   });
 
   // POST route for saving a new post
-  app.post("/api/reviews", function(req,res) {
+  app.post("/api/reviews", function (req, res) {
     // Write code here to create a new review and save it to the database
     var review = req.body;
     db.Review.create({
-      review:review.review,
-      score:review.score,
-      title:review.title,
+      review: review.review,
+      score: review.score,
+      title: review.title,
     })
-      .then(function(dbReview) {
+      .then(function (dbReview) {
         res.json(dbReview);
       });
   });
